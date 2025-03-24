0781bf73926ae47e20d0a628250104c6b168b7c0
Arthur Carvalho Rodrigues <1109178@sga.pucminas.br>
2025-03-24 16:29:43 -0300

    Added base logic for result_exporter.py (not completed

--- autograder.py
+++ autograder.py
@@ -1,12 +1,13 @@
 import argparse
 from grading.final_scorer import get_final_score
+from utils.result_exporter import export
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Github Classroom HTML/CSS/JS autograder by Webtech Network")
     parser.add_argument("--html-weight", type=float, required=True, help="Weight for HTML grading")
     parser.add_argument("--css-weight", type=float, required=True, help="Weight for CSS grading")
     parser.add_argument("--js-weight", type=float, required=True, help="Weight for JavaScript grading")
-    #parser.add_argument("--grading-criteria", type=str, required=True, help="JSON string with grading criteria")
-    #parser.add_argument("--timeout", type=int, required=True, help="Timeout for grading execution in seconds")
+    parser.add_argument("--grading-criteria", type=str, required=True, help="JSON string with grading criteria")
+    parser.add_argument("--timeout", type=int, required=True, help="Timeout for grading execution in seconds")
 
     return parser.parse_args()
 
@@ -17,13 +18,16 @@ if __name__ == "__main__":
     html_weight = args.html_weight
     css_weight = args.css_weight
     js_weight = args.js_weight
-    #grading_criteria = args.grading_criteria
-    #timeout = args.timeout
+    grading_criteria = args.grading_criteria
+    timeout = args.timeout
     if html_weight+css_weight+js_weight != 100:
         raise ValueError("Weights must sum to 100")
 
     final_score = get_final_score(html_weight,css_weight,js_weight)
 
     print(f"Final score is {final_score}")
+
+    export(final_score)
+
 
 
--- utils/result_exporter.py
+++ utils/result_exporter.py
@@ -1,3 +1,7 @@
+import os
+import json
+import base64
+
 def create_dict(final_score):
     status = "passed" if final_score >= 60 else "failed"
     result = {
@@ -9,11 +13,28 @@ def create_dict(final_score):
                 "status": "pass",
                 "test_code": 'assert "background-color" in css, "No background color set for <body>."',
                 "task_id": 0,
-                "filename": "tests/test_css.py",
+                "filename": "autograder.py",
                 "line_no": 7,
                 "duration": 0.0001458939999992026,
-                "score": 11.11111111111111
+                "score": final_score
             }
         ],
         "max_score": 100
     }
+
+    return result
+
+def encode_result(result):
+    encoded_data = base64.b64encode(json.dumps(result).encode()).decode()
+    return encoded_data
+def export_result(encoded_data):
+    github_output = os.getenv("GITHUB_OUTPUT")
+    if github_output:
+        with open(github_output, "a") as f:
+            f.write(f"result={encoded_data}\n")
+
+
+def export(final_score):
+    result_dict = create_dict(final_score)
+    result_dict = encode_result(result_dict)
+    export_result(result_dict)
