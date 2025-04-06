110ad1c76227ff2ccc559af7e181d6e8ce373d15
ArthurSGA <arodrigues@sga.pucminas.br>
2025-04-06 14:49:59 -0300

    Disabled terminal output for python tests.

--- grading/grader.py
+++ grading/grader.py
@@ -3,10 +3,9 @@ from utils.collector import TestCollector
 
 def grade(test_file: str, total_tests: int):
     collector = TestCollector()
-    result = pytest.main([test_file,'--disable-warnings','--capture=no'],plugins=[collector])
+    result = pytest.main([test_file,"-p", "no:terminal"],plugins=[collector])
     passed_tests =  collector.passed
     score = (passed_tests/ total_tests) * 100
     return score
-
 
 
