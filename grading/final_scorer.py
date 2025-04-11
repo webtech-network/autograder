7a8db1bda06d34fb896dbebbbabe7dfc8d039a85
ArthurSGA <arodrigues@sga.pucminas.br>
2025-04-11 16:04:50 -0300

    Pretty important refactoring here. Implemented Scorer class: - Stores base/bonus/penalty tests results. - Handles grader.py methods and assign it to it's attributes. - Fabric method to efficiently generate a complete Scorer obj.

--- grading/final_scorer.py
+++ grading/final_scorer.py
@@ -1,18 +1,31 @@
-from grading.grader import grade
+from grading.grader import get_test_results,get_score
 from utils.path import Path
+from time import sleep
 
-def get_final_score(html_weight,css_weight,js_weight):
-    path = Path(__file__,'tests')
-    html_score = grade(path.getFilePath('test_html.py'),24)
-    css_score = grade(path.getFilePath('test_css.py'),11)
-    js_score = grade(path.getFilePath('test_js.py'),15)
-    final_score = (
-        ((html_score * html_weight))+
-        ((css_score * css_weight)) + 
-        ((js_score * js_weight)))/100
-    return final_score
+class Scorer:
+    def __init__(self,test_folder):
+        self.path = Path(__file__,test_folder)
+        self.base = ()
+        self.bonus = ()
+        self.penalty = ()
+        self.final_score = 0
+    def set_base_score(self,filename):
+        self.base = get_test_results(self.path.getFilePath(filename))
+    def set_bonus_score(self,filename):
+        self.bonus = get_test_results(self.path.getFilePath(filename))
+    def set_penalty_score(self,filename):
+        self.penalty = get_test_results(self.path.getFilePath(filename))
+    def set_final_score(self):
+        final_score = (get_score(self.base[0],9) * 0.8 )+(get_score(self.bonus[0],4) * 0.2) - (get_score(self.penalty[0],5) * 0.3)
+        self.final_score = final_score
+        return final_score
 
-def test_path(weight):
-    return f"Weight is {weight}"
-
-
+    @classmethod
+    def create_with_scores(cls,test_folder,base_file,bonus_file,penalty_file):
+        scorer = cls(test_folder)
+        scorer.set_base_score(base_file)
+        scorer.set_bonus_score(bonus_file)
+        scorer.set_penalty_score(penalty_file)
+        scorer.set_final_score()
+        sleep(2)
+        return scorer
