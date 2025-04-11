81057605775ab99159cbc052b6b5bc850cd81a01
ArthurSGA <arodrigues@sga.pucminas.br>
2025-04-11 16:25:56 -0300

    Implemented generate_md method to Scorer to make the report generation efficient.

--- grading/final_scorer.py
+++ grading/final_scorer.py
@@ -1,4 +1,5 @@
 from grading.grader import get_test_results,get_score
+from utils.report_generator import generate_md
 from utils.path import Path
 from time import sleep
 
@@ -19,6 +20,12 @@ class Scorer:
         final_score = (get_score(self.base[0],9) * 0.8 )+(get_score(self.bonus[0],4) * 0.2) - (get_score(self.penalty[0],5) * 0.3)
         self.final_score = final_score
         return final_score
+    def get_feedback(self):
+        def get_feedback(self):
+            base_dict = {"passed": self.base[0], "failed": self.base[1]}
+            bonus_dict = {"passed": self.bonus[0], "failed": self.bonus[1]}
+            penalty_dict = {"passed": self.penalty[0], "failed": self.penalty[1]}
+            return generate_md(base_dict, bonus_dict, penalty_dict, self.final_score, self.author)
 
     @classmethod
     def create_with_scores(cls,test_folder,base_file,bonus_file,penalty_file):
