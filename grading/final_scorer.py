d4e9896ca0e9c423d5e7406d898e3d8bddc716c8
ArthurSGA <arodrigues@sga.pucminas.br>
2025-04-14 13:19:17 -0300

    Finished implementing new Config and Grader features on Scorer. New input criteria configuration feature is now done!!

--- grading/final_scorer.py
+++ grading/final_scorer.py
@@ -1,37 +1,46 @@
 from grading.grader import Grader
 from utils.report_generator import generate_md
 from utils.path import Path
+from utils.config_loader import Config
 from time import sleep
 
 class Scorer:
-    def __init__(self,test_folder,author):
+    def __init__(self,test_folder,author,config):
         self.path = Path(__file__,test_folder)
         self.author = author
+        self.config = config
         self.base_grader = None
         self.bonus_grader = None
         self.penalty_grader = None
         self.final_score = 0
+
     def set_base_score(self,filename):
-        self.base_grader = Grader.create(self.path.getFilePath(filename))
+        self.base_grader = Grader.create(self.path.getFilePath(filename),self.config.base_config)
+
     def set_bonus_score(self,filename):
-        self.bonus_grader = Grader.create(self.path.getFilePath(filename))
+        self.bonus_grader = Grader.create(self.path.getFilePath(filename),self.config.bonus_config)
+
     def set_penalty_score(self,filename):
-        self.penalty_grader = Grader.create(self.path.getFilePath(filename))
+        self.penalty_grader = Grader.create(self.path.getFilePath(filename),self.config.penalty_config)
+
     def set_final_score(self):
-        final_score = (self.base_grader.get_score() * 0.8 )+(self.bonus_grader.get_score() * 0.2) - (self.penalty_grader.get_score() * 0.3)
+        final_score = (self.base_grader.generate_score())+(self.bonus_grader.generate_score()) - (self.penalty_grader.generate_score())
         self.final_score = final_score
         return final_score
+
     def get_feedback(self):
         base_dict = {"passed": self.base_grader.passed_tests, "failed": self.base_grader.failed_tests}
         bonus_dict = {"passed": self.bonus_grader.passed_tests, "failed": self.bonus_grader.failed_tests}
         penalty_dict = {"passed": self.penalty_grader.passed_tests, "failed": self.penalty_grader.failed_tests}
         return generate_md(base_dict, bonus_dict, penalty_dict, self.final_score, self.author)
+
     def create_feedback(self):
         with open(self.path.getFilePath("feedback.md"),'w',encoding="utf-8") as feedback:
             feedback.write(self.get_feedback())
+
     @classmethod
-    def create_with_scores(cls,test_folder,author,base_file,bonus_file,penalty_file):
-        scorer = cls(test_folder,author)
+    def create_with_scores(cls,test_folder,author, config: Config ,base_file,bonus_file,penalty_file):
+        scorer = cls(test_folder,author,config)
         scorer.set_base_score(base_file)
         scorer.set_bonus_score(bonus_file)
         scorer.set_penalty_score(penalty_file)
@@ -40,8 +49,8 @@ class Scorer:
         return scorer
 
     @classmethod
-    def quick_build(cls, author):
-        scorer = Scorer.create_with_scores("tests", author, "test_base.py", "test_bonus.py", "test_penalty.py")
+    def quick_build(cls, author,config):
+        scorer = Scorer.create_with_scores("tests", author,config, "test_base.py", "test_bonus.py", "test_penalty.py")
         return scorer
 
 if __name__ == '__main__':
