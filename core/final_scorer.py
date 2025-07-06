98fd966aa4fa0ec98cd962b3cccd1bf53d041e85
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-07-06 14:21:57 -0300

    feat: Adjusted `Scorer` to deal with the `Reporter` classes and `Result` class.

--- core/final_scorer.py
+++ core/final_scorer.py
@@ -9,15 +9,13 @@ from core.report.reporter import Reporter
 
 class Scorer:
     """This class is used to manage the grading process for the three test suites: base, bonus, and penalty."""
-    def __init__(self,test_folder,author, github_token):
+    def __init__(self,test_folder,author):
         self.path = Path(__file__,test_folder) # Path to the test folder
         self.author = author # Author of the code being graded
-        self.github_token = github_token # Account id of the author
         self.config = None # Config instance containing the configurations for all the test files
         self.base_grader = None # Grader instance for the base test file
         self.bonus_grader = None # Grader instance for the bonus test file
         self.penalty_grader = None # Grader instance for the penalty test file
-        self.final_score = 0  # Final score after grading all test files
 
     def set_base_score(self,filename):
         """Set the base score by creating a Grader instance for the base test file."""
@@ -31,46 +29,44 @@ class Scorer:
         """Set the penalty score by creating a Grader instance for the penalty test file."""
         self.penalty_grader = Grader.create(f"{filename}",self.config.penalty_config)
 
-    def set_final_score(self):
+    def get_final_score(self):
         """Calculate the final score by combining the scores from base, bonus, and penalty test files."""
         base_score = self.base_grader.generate_score() # Generate the score for the base test file
-        self.final_score = base_score
+        final_score = base_score
         if base_score < 100:
-            self.give_bonus_score(base_score) # If the base score is less than 100, add the bonus score
+            final_score = self.give_bonus_score(base_score,final_score) # If the base score is less than 100, add the bonus score
         penalty_score = self.penalty_grader.generate_score() # Generate the score for the penalty test file
-        self.final_score -= penalty_score # Subtract the penalty score from the final score
-        return self.final_score
+        final_score -= penalty_score # Subtract the penalty score from the final score
+        return final_score
 
-    def give_bonus_score(self,base_score):
+    def give_bonus_score(self,base_score,final_score):
         """Add the bonus score to the final score if the base score is less than 100."""
         bonus_score = self.bonus_grader.generate_score()
         if 100 - base_score >= self.bonus_grader.test_config.weight:
-            self.final_score += bonus_score
+            final_score += bonus_score
         elif 100 - base_score < self.bonus_grader.test_config.weight:
-            self.final_score += (bonus_score/self.bonus_grader.test_config.weight) * (100 - base_score) # If the bonus score exceeds the remaining points to 100, scale it down
+            final_score += (bonus_score/self.bonus_grader.test_config.weight) * (100 - base_score) # If the bonus score exceeds the remaining points to 100, scale it down
+        return final_score
 
-    def get_feedback(self):
-        """Generate feedback in Markdown format based on the test results."""
-        base_dict = {"passed": self.base_grader.passed_tests, "failed": self.base_grader.failed_tests} # Format the base test results
-        bonus_dict = {"passed": self.bonus_grader.passed_tests, "failed": self.bonus_grader.failed_tests} # Format the bonus test results
-        penalty_dict = {"passed": self.penalty_grader.passed_tests, "failed": self.penalty_grader.failed_tests} # Format the penalty test results
-        return generate_md(base_dict, bonus_dict, penalty_dict, self.final_score, self.author) # Calls the generate_md function to create the feedback
-    def get_ai_feedback(self):
-        """Generate AI Generated feedback in Markdown format based on the test results."""
-        code = self.get_student_files()
-        base_dict = {"passed": self.base_grader.passed_tests,"failed": self.base_grader.failed_tests}  # Format the base test results
-        bonus_dict = {"passed": self.bonus_grader.passed_tests,"failed": self.bonus_grader.failed_tests}  # Format the bonus test results
-        penalty_dict = {"passed": self.penalty_grader.passed_tests,"failed": self.penalty_grader.failed_tests}  # Format the penalty test results
-        return generate_ai_md(code, base_dict, bonus_dict, penalty_dict, self.final_score,self.author)  # Calls the generate_md function to create the feedback
-    def create_feedback(self):
-        """Create a feedback file in Markdown format with the test results and final score."""
-        allowed = decrement_token_quota(self.github_token)
+    def generate_result(self):
+        """Generate a Result instance with the grading results"""
+        final_score = self.get_final_score()
+        base_dict = {"passed": self.base_grader.passed_tests,
+                     "failed": self.base_grader.failed_tests}  # Format the base test results
+        bonus_dict = {"passed": self.bonus_grader.passed_tests,
+                      "failed": self.bonus_grader.failed_tests}  # Format the bonus test results
+        penalty_dict = {"passed": self.penalty_grader.passed_tests,
+                        "failed": self.penalty_grader.failed_tests}  # Format the penalty test results
+        return Result(final_score,self.author,self.get_student_files(),base_dict, bonus_dict, penalty_dict)
 
-        with open("feedback.md",'w',encoding="utf-8") as feedback:
+    def get_reporter(self,token,mode="default"):
+        """Creates a Reporter instance with the students results"""
+        result = self.generate_result()
+        if mode == "ai":
+            allowed = decrement_token_quota(self.author)
             if allowed:
-                feedback.write(self.get_ai_feedback())
-            else:
-                feedback.write(self.get_feedback())
+                return Reporter.create_ai_reporter(result,token)
+        return Reporter.create_default_reporter(result,token)
 
     def get_student_files(self):
         """Get the student files."""
@@ -78,18 +74,18 @@ class Scorer:
             return student_file.read()
 
     @classmethod
-    def create_with_scores(cls,test_folder,github_token,author, config_file ,base_file,bonus_file,penalty_file):
+    def create_with_scores(cls,test_folder,author, config_file ,base_file,bonus_file,penalty_file):
         """Create a Scorer instance with the specified test files and author."""
-        scorer = cls(test_folder,author, github_token)
+        scorer = cls(test_folder,author)
 
-        if not token_exists(scorer.github_token):
-            create_token(scorer.github_token)
+        if not token_exists(scorer.author):
+            create_token(scorer.author,10)
 
         scorer.config = Config.create_config(config_file) # Load the configuration from the specified file
         scorer.set_base_score(base_file)
         scorer.set_bonus_score(bonus_file)
         scorer.set_penalty_score(penalty_file)
-        scorer.set_final_score()
+        #scorer.set_final_score()
         sleep(2)
         return scorer
 
--- core/report/ai_reporter.py
+++ core/report/ai_reporter.py
@@ -71,3 +71,4 @@ class AIReporter(BaseReporter):
         return feedback
 
 
+
