5d9bd83ee106da554d75a1000025d630b08be256
ArthurSGA <arodrigues@sga.pucminas.br>
2025-07-06 23:51:31 -0300

    refactor: Refactored autograder.py to work with the new system architecture

--- autograder.py
+++ autograder.py
@@ -1,7 +1,5 @@
 from core.final_scorer import Scorer
 import argparse
-from core.report.result_exporter import notify_classroom
-from core.report.commit_report import overwrite_report_in_repo
 import os
 
 parser = argparse.ArgumentParser(description="Process token argument.")
@@ -12,13 +10,17 @@ github_token = args.token
 author = os.getenv("GITHUB_ACTOR")
 
 
-scorer = Scorer.create_with_scores("tests",github_token,author,"test_base.py","test_bonus.py","test_penalty.py")
-final_score = scorer.final_score
+scorer = Scorer.quick_build(author)
 
 
-feedback = scorer.create_feedback()
-overwrite_report_in_repo(github_token,new_content=feedback)
+reporter = scorer.get_reporter(github_token, mode="ai")
+reporter.create_report()
+reporter.notify_classroom()
+reporter.overwrite_report_in_repo(github_token)
 
-notify_classroom(final_score, github_token)
+
+
+
+
 
 
