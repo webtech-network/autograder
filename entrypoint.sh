083dab8492d481c102c1c303442ff2253eaf338a
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-07-16 16:02:47 -0300

    feat: Configuring AI Report for etapa 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -114,7 +114,7 @@ fi
 python tests/result_parser.py
 
 # --- Run the autograder ---
-python main.py  --token $1 --redis-token $2 --redis-url $3 --openai-key $4
+python autograder.py  --token $1 --redis-token $2 --redis-url $3 --openai-key $4
 
 echo "Autograding completed successfully!"
 echo "Final results generated and sent to GitHub Classroom!"
