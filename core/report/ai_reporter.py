dd59ef740c6d6cc77facee138a8080b72effb57b
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-07-16 13:11:48 -0300

    feat: Added anchor to discord

--- core/report/ai_reporter.py
+++ core/report/ai_reporter.py
@@ -98,6 +98,7 @@ class AIReporter(BaseReporter):
         feedback += f"# Feedback para {self.result.author}:\n\n"
         feedback += f"Nota final: **{self.result.final_score:.1f}/100**\n\n"
         feedback += response.choices[0].message.content
+        feedback += "> Caso queira tirar uma dúvida específica, entre em contato com o Chapter no nosso (discord)[https://discord.gg/gTUbnPgj].\n\n"
         feedback += "\n\n---\n"
         feedback += "<sup>Made By the Autograder Team.</sup><br>&nbsp;&nbsp;&nbsp;&nbsp;<sup><sup>- [Arthur Carvalho](https://github.com/ArthuCRodrigues)</sup></sup><br>&nbsp;&nbsp;&nbsp;&nbsp;<sup><sup>- [Arthur Drumond](https://github.com/drumondpucminas)</sup></sup><br>&nbsp;&nbsp;&nbsp;&nbsp;<sup><sup>- [Gabriel Resende](https://github.com/gnvr29)</sup></sup>"
         return feedback
