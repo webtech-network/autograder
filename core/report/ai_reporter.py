898ec1ff885e308dc2bd5e5c61bc0328b13f448d
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-07-16 22:53:49 -0300

    fix: fixed report formatting

--- core/report/ai_reporter.py
+++ core/report/ai_reporter.py
@@ -100,7 +100,7 @@ class AIReporter(BaseReporter):
         feedback += response.choices[0].message.content
         feedback += "\n\n> Caso queira tirar uma dúvida específica, entre em contato com o Chapter no nosso [discord](https://discord.gg/gTUbnPgj).\n\n"
         feedback += "\n\n---\n"
-        feedback += "<sup>Made By the Autograder Team.</sup><br>&nbsp;&nbsp;&nbsp;&nbsp;<sup><sup>- [Arthur Carvalho](https://github.com/ArthuCRodrigues)</sup></sup><br>&nbsp;&nbsp;&nbsp;&nbsp;<sup><sup>- [Arthur Drumond](https://github.com/drumondpucminas)</sup></sup><br>&nbsp;&nbsp;&nbsp;&nbsp;<sup><sup>- [Gabriel Resende](https://github.com/gnvr29)</sup></sup>"
+        feedback += "<sup>Made By the Autograder Team.</sup><br>&nbsp;&nbsp;&nbsp;&nbsp;<sup><sup>- [Arthur Carvalho](https://github.com/ArthurCRodrigues)</sup></sup><br>&nbsp;&nbsp;&nbsp;&nbsp;<sup><sup>- [Arthur Drumond](https://github.com/drumondpucminas)</sup></sup><br>&nbsp;&nbsp;&nbsp;&nbsp;<sup><sup>- [Gabriel Resende](https://github.com/gnvr29)</sup></sup>"
         return feedback
 
     @classmethod
