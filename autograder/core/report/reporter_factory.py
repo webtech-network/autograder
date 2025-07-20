14bfbfd8412ea0c36c05c90e15ca344eb78669a9
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-07-20 13:31:21 -0300

    refactor: renamed reporter.py to reporter_factory.py

--- autograder/core/report/reporter_factory.py
+++ autograder/core/report/reporter_factory.py
@@ -2,13 +2,13 @@ from autograder.core.report.ai_reporter import AIReporter
 from autograder.core.report.default_reporter import DefaultReporter
 class Reporter:
     @classmethod
-    def create_ai_reporter(cls, result,token,quota, openai_key=None):
+    def create_ai_reporter(cls, result,openai_key,quota):
         """Creates an AIReporter instance with the students results"""
-        return AIReporter.create(result,token,quota,openai_key)
+        return AIReporter.create(result,quota,openai_key)
 
     @classmethod
-    def create_default_reporter(cls, result,token):
+    def create_default_reporter(cls, result):
         """Creates a DefaultReporter instance with the students results"""
-        return DefaultReporter.create(result, token)
+        return DefaultReporter.create(result)
 
 
