2c3a2360d22370161fbc7717536b5197113cd05f
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-07-20 08:32:34 -0300

    refactor: folder name refactor in entrypoint-dev.sh

--- entrypoint-dev.sh
+++ entrypoint-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-npm test -- --json --outputFile=./tests/test-results.json
+npm test -- --json --outputFile=./validation/test-results.json
 
 echo "Parsing tests..."
-python tests/result_parser.py
+python validation/result_parser.py
