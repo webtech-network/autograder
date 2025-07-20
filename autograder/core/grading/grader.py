dd155825095021d6ce36bdd663dea6cc05f6c1ba
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-07-20 08:30:31 -0300

    refactor: grader.py refactor (positon,name,removed compositions)

--- autograder/core/grading/grader.py
+++ autograder/core/grading/grader.py
@@ -1,20 +1,30 @@
-from abc import ABC, abstractmethod
-from core.config_processing.test_config import TestConfig
-from core.grading.subject_grader import SubjectGrader
 
-class BaseGrader(ABC):
+from autograder.core.config_processing.test_config import TestConfig
+from autograder.core.grading.result_processor import ResultProcessor
+from autograder.core.grading.subject_grader import SubjectGrader
+
+class Grader:
     """Abstract base class for grading"""
     def __init__(self, test_file,test_config):
         self.test_file = test_file
-        self.test_amount = 0  # Total number of tests in the test file
+        self.test_amount = 0  # Total number of validation in the test file
         self.test_config = test_config  # TestConfig instance containing the configuration for the test file
-        self.passed_tests = []  # List of passed tests
-        self.failed_tests = []  # List of failed tests
-        self.quantitative_results = {}  # Dictionary to store quantitative results from tests
+        self.passed_tests = []  # List of passed validation
+        self.failed_tests = []  # List of failed validation
+        self.quantitative_results = {}  # Dictionary to store quantitative results from validation
 
-    @abstractmethod
     def get_test_results(self):
-        pass
+        """Get the validation results from a json results file"""
+        results_dict = ResultProcessor.load_results(f"validation/results/{self.test_file.split('.')[0]}_results.json")  # Load the test results from the specified file
+        passed_tests = [test['test'] for test in results_dict[0]]
+        failed_tests = [test['test'] for test in results_dict[1]]
+        quantitative_tests = []
+        self.passed_tests = passed_tests  # Set the passed validation
+        self.failed_tests = failed_tests
+        if len(results_dict) > 2:  # If there are quantitative results, set them
+            quantitative_tests = {test['test']: test['count'] for test in results_dict[2]}
+            self.quantitative_results = quantitative_tests
+
 
     def generate_score(self):
         """Generate the score based on the test results and the test configuration."""
