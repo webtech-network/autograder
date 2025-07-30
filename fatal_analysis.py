eb7f540c98a4d41250c08dd5b6176fdf5c5cbb16
Gabriel Nogueira Vieira Resende <gabrielnvresende@gmail.com>
2025-07-30 19:25:08 -0300

    feat: added extra fatal tests

--- fatal_analysis.py
+++ fatal_analysis.py
@@ -14,8 +14,11 @@ FEEDBACK_MAPPING = {
     'package_json_has_main_key': '- 🔑 A chave `"main"` está faltando no seu `package.json`. Ela é necessária para indicar ao Node.js qual arquivo executar.\n',
     'package_json_main_is_correct': '- 🎯 O script `"main"` no seu `package.json` está incorreto. Ele deve ser `"server.js"`.\n',
     'package_json_has_express_dependency': '- 🚀 O pacote `express` não foi encontrado nas dependências do seu `package.json`. Ele é essencial para o servidor.\n',
-    'db_container_running': 'Não conseguimos rodar o banco de dados para os testes. Comunique a equipe de desenvolvimento do autograder.',
-    'db_connectivity': 'Não conseguimos conectar ao banco de dados para os testes. Comunique a equipe de desenvolvimento.'
+    'db_container_running': '- 🐳 Ocorreu um problema ao iniciar o contêiner do banco de dados. Por favor, contate o time de desenvolvimento do autograder.\n',
+    'db_connectivity': '- 🔌 Não foi possível estabelecer uma conexão com o banco de dados. Por favor, contate o time de desenvolvimento do autograder.\n',
+    'migrations_created': '-  migrating... ERROR. Falha ao criar o arquivo de migration. Isso geralmente ocorre se um arquivo com o mesmo nome já existe ou por um erro no Knex.\n',
+    'seeds_created': '- seeding... ERROR. Falha ao criar o arquivo de seed. Verifique se um arquivo de seed com o mesmo nome já foi criado.\n',
+    'seeds_running': '- seeding... RUNNING... ERROR. Falha ao executar as seeds. Verifique seus arquivos de seed em busca de erros de sintaxe SQL ou se as tabelas foram criadas corretamente pelas migrations.\n'
 }
 
 BASE_DIR = os.path.join(os.environ.get('GITHUB_WORKSPACE', ''), 'submission')
@@ -24,15 +27,30 @@ def check_server_status(errors):
     if server_status == "1":
         errors.append('server_js_invalid')
 
-def db_container_status(errors):
+def check_db_container_status(errors):
     db_container_status = os.environ.get('DATABASE_CONTAINER_STATUS', '0')
     if db_container_status == "1":
         errors.append('db_container_running')
 
-def db_status(errors):
+def check_db_status(errors):
     db_conn_status = os.environ.get('DATABASE_STATUS', '0')
     if db_conn_status == "1":
         errors.append('db_connectivity')
+
+def check_migrations_status(errors):
+    migrations_status = os.environ.get('MIGRATIONS_STATUS', '0')
+    if migrations_status == "1":
+        errors.append('migrations_created')
+
+def check_seeds_creation_status(errors):
+    seeds_creation_status = os.environ.get('SEEDS_CREATION_STATUS', '0')
+    if seeds_creation_status == "1":
+        errors.append('seeds_created')
+
+def check_seeds_running_status(errors):
+    seeds_running_status = os.environ.get('SEEDS_RUN_STATUS', '0')
+    if seeds_running_status == "1":
+        errors.append('seeds_running')
 
 def check_server_js_exists(errors):
     path = os.path.join(BASE_DIR, 'server.js')
@@ -71,6 +89,19 @@ def main():
     package_json = check_package_json_exists(errors)
     if package_json:
         check_package_json_content(package_json, errors)
+    
+    #Checks if the container started and the database connection
+    check_db_container_status(errors)
+    check_db_status(errors)
+
+    #Checks for migrations creation status
+    check_migrations_status(errors)
+
+    #Checks for seeds creation status
+    check_seeds_creation_status(errors)
+
+    #Checks for seeds running status
+    check_seeds_running_status(errors)
 
     # Handle errors
     if errors:
