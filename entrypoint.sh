50eae75ffc99c23579a7dd6c07def92f22ae29b3
Gabriel Nogueira Vieira Resende <gabrielnvresende@gmail.com>
2025-07-30 19:22:52 -0300

    fix: fixed entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,99 @@ set -e
 # Print a message to indicate the start of the autograding process
 echo "Starting autograder..."
 
-# --- Install dependencies in the student's repository and run server.js ---
+#Switches to autograder directory and loads environment variables from .env file
+cd /app
+source .env
+
+# --- Starting database container --- #
+DATABASE_CONTAINER_STATUS=1
+CONTAINER_NAME="pg-test-db"
+
+echo "Starting PostgreSQL container"
+docker compose up -d
+DATABASE_CONTAINER_STATUS=$?
+
+export DATABASE_CONTAINER_STATUS;
+
+if [ $DATABASE_CONTAINER_STATUS -ne 0 ]; then
+    echo "Database container responded with status code: $DATABASE_CONTAINER_STATUS. Database is up and recheable"
+else
+    echo "Database container responded with status code: $DATABASE_CONTAINER_STATUS. Database failed to start"
+fi
+
+# --- Checking for database initialization --- #
+DATABASE_STATUS=1
+MAX_DB_CONNECTION_ATTEMPTS=10
+DB_SERVICE_NAME="postgres-database"
+SLEEP_INTERVAL=3
+
+if [ $DATABASE_CONTAINER_STATUS -eq 0 ]; then
+    echo "Checking for PostgreSQL database's connectivity..."
+    for i in $(seq 1 $MAX_DB_CONNECTION_ATTEMPTS);
+    do
+        echo "Attempt $i/$MAX_DB_CONNECTION_ATTEMPTS"
+        
+        docker exec -T $DB_SERVICE_NAME pg_isready -U $POSTGRES_USER -q
+        DATABASE_STATUS=$?
+
+        if [ $DATABASE_STATUS -eq 0 ]; then
+            echo "PostgreSQL database is successfully accepting connections"
+            break;
+        fi
+
+        if [ $i -eq $MAX_DB_CONNECTION_ATTEMPTS ]; then
+            echo "PostgreSQL database is not accepting connections"
+        else 
+            sleep $SLEEP_INTERVAL
+        fi
+    done
+fi
+
+export DATABASE_STATUS
+
+# --- Install dependencies in the student's repository ---
 cd "$GITHUB_WORKSPACE/submission"
 
 
 if [ -f "package.json" ]; then
     echo "Downloading dependencies from student's project"
-    npm install;
+    npm ci;
 else
     echo "Error: no package.json file found."
     exit 1;
 fi
+
+# --- Creates migrations --- #
+MIGRATIONS_STATUS=0
+MIGRATIONS_DIR="./db/migrations"
+if ls "$MIGRATIONS_DIR"/*.js >/dev/null 2>&1; then
+    echo "✅ Migration files already exist, skipping creation."
+else
+    npx knex migrate:make solution_migrations.js
+    MIGRATIONS_STATUS=$?
+fi
+
+export MIGRATIONS_STATUS
+
+# --- Creates and runs seeds --- #
+SEEDS_CREATION_STATUS=0
+SEEDS_DIR="./db/seeds"
+
+if ls "$MIGRATIONS_DIR"/*.js >/dev/null 2>&1; then
+    echo "✅ Migration files already exist, skipping creation."
+else
+    npx knex seed:make *_solution_migrations.js
+    SEEDS_CREATION_STATUS=$?
+fi
+
+export SEEDS_CREATION_STATUS
+
+npx knex seed:run
+SEEDS_RUN_STATUS=$?
+
+export SEEDS_RUN_STATUS
+
+# --- Runs server --- #
 
 echo "Starting server.js at port 3000..."
 node server.js &
@@ -90,55 +172,7 @@ fi
 
 tree -I 'node_modules'
 
-#Switches to autograder directory and loads environment variables from .env file
 cd /app
-source .env
-
-# --- Starting database container --- #
-DATABASE_CONTAINER_STATUS=1
-CONTAINER_NAME="pg-test-db"
-
-echo "Starting PostgreSQL container"
-docker compose up -d
-DATABASE_CONTAINER_STATUS=$?
-
-export DATABASE_CONTAINER_STATUS;
-
-if [ $DATABASE_CONTAINER_STATUS -ne 0 ]; then
-    echo "Database container responded with status code: $DATABASE_CONTAINER_STATUS. Database is up and recheable"
-else
-    echo "Database container responded with status code: $DATABASE_CONTAINER_STATUS. Database failed to start"
-fi
-
-# --- Checking for database initialization --- #
-DATABASE_STATUS=1
-MAX_DB_CONNECTION_ATTEMPTS=10
-DB_SERVICE_NAME="postgres-database"
-SLEEP_INTERVAL=3
-
-if [ $DATABASE_CONTAINER_STATUS -eq 0 ]; then
-    echo "Checking for PostgreSQL database's connectivity..."
-    for i in $(seq 1 $MAX_DB_CONNECTION_ATTEMPTS);
-    do
-        echo "Attempt $i/$MAX_DB_CONNECTION_ATTEMPTS"
-        
-        docker exec -T $DB_SERVICE_NAME pg_isready -U $POSTGRES_USER -q
-        DATABASE_STATUS=$?
-
-        if [ $DATABASE_STATUS -eq 0 ]; then
-            echo "PostgreSQL database is successfully accepting connections"
-            break;
-        fi
-
-        if [ $i -eq $MAX_DB_CONNECTION_ATTEMPTS ]; then
-            echo "PostgreSQL database is not accepting connections"
-        else 
-            sleep $SLEEP_INTERVAL
-        fi
-    done
-fi
-
-export DATABASE_STATUS
 
 # --- Running fatal analysis
 
