4ee8a5464764ebe731d66d79885d9a1ae983a1cd
Gabriel Nogueira Vieira Resende <gabrielnvresende@gmail.com>
2025-07-31 07:33:58 -0300

    feat: added timeout error handling in super test

--- tests/setup.js
+++ tests/setup.js
@@ -5,6 +5,8 @@ function safeTest(description, asyncTestFn) {
         } catch (error) {
             if (error.code === 'ECONNREFUSED') {
                 throw new Error(`Connection refused on test: "${description}". Is the server running?`);
+            } else if (error.code === 'ECONNABORTED'){
+                throw new Error(`There was a timeout on test: "${description}". Test it locally and try again`)
             }
             throw new Error(`An unexpected error occurred in test "${description}": ${error.message}`);
         }
