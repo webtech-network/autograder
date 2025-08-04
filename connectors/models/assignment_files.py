ac9a2e6e8dbee0b3b192e4f610af5844a05cda7c
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-08-04 15:56:00 -0300

    fix: Fixed parameter name in assignment_files.py

--- connectors/models/assignment_files.py
+++ connectors/models/assignment_files.py
@@ -2,8 +2,8 @@ class AssignmentFiles:
     """
     Keeps all the configuration files for an assignment.
     """
-    def __init__(self,submission_files,criteira,feedback,ai_feedback=None):
-        self.submission_files = submission_files
-        self.criteria = criteira
+    def __init__(self,submission_files,criteria,feedback,ai_feedback=None):
+        self.submission_files = submission_files #dict of file names to file contents
+        self.criteria = criteria
         self.feedback = feedback
         self.ai_feedback = ai_feedback
