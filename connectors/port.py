8376847e20a01ada7de6dac3a2b7ce5f3fba77b9
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-08-04 23:02:39 -0300

    feat: updated port.py to work with AutograderRequest obj

--- connectors/port.py
+++ connectors/port.py
@@ -6,32 +6,15 @@ from autograder.autograder_facade import Autograder
 
 
 class Port(ABC):
-
     """
     Abstract Port class that defines the accepted interface for the core system communication.
     """
-    def __init__(self,test_framework,grading_preset,student_name,student_credentials,feedback_type,openai_key=None,redis_url=None,redis_token=None):
-        self.test_framework = test_framework
-        self.grading_preset = grading_preset
-        self.student_name = student_name
-        self.student_credentials = student_credentials
-        self.feedback_type = feedback_type
-        self.openai_key = openai_key
-        self.redis_url = redis_url
-        self.redis_token = redis_token
+    def __init__(self):
+        self.autograder_request = None
         self.autograder_response = None
-
     async def run_autograder(self):
         try:
-            response = await Autograder.grade(
-                test_framework=self.test_framework,
-                student_name=self.student_name,
-                student_credentials=self.student_credentials,
-                feedback_type=self.feedback_type,
-                openai_key=self.openai_key,
-                redis_url=self.redis_url,
-                redis_token=self.redis_token
-            )
+            response = await Autograder.grade(self.autograder_request)
             self.autograder_response = response
             return self
         except Exception as e:
