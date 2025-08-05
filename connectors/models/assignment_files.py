832268134e858046e7793153425e848616fe74f2
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-08-04 23:01:13 -0300

    chore: deleted assignment_files.py

--- connectors/models/assignment_files.py
+++
@@ -1,9 +0,0 @@
-class AssignmentFiles:
-    """
-    Keeps all the configuration files for an assignment.
-    """
-    def __init__(self,submission_files,criteria,feedback,ai_feedback=None):
-        self.submission_files = submission_files #dict of file names to file contents
-        self.criteria = criteria
-        self.feedback = feedback
-        self.ai_feedback = ai_feedback
