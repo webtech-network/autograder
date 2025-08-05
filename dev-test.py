66f7340ee1ec1d721cf80ef42a8733226a2b48ac
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-08-05 10:01:26 -0300

    refactor: removing useless files

--- dev-test.py
+++
@@ -1,6 +0,0 @@
-from autograder.core.grading.scorer import Scorer
-
-scorer = Scorer.quick_build("")
-
-final_score = scorer.final_score
-print(final_score)
--- entrypoint-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-npm test -- --json --outputFile=./validation/test-results.json
-
-echo "Parsing tests..."
-python validation/result_parser.py
