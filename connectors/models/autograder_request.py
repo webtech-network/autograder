35a28529291b171556be851bc2432e9906133e58
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-08-05 09:50:00 -0300

    refactor: renaming variable names

--- connectors/models/autograder_request.py
+++ connectors/models/autograder_request.py
@@ -1,22 +1,22 @@
-from connectors.models.assignment_config import Preset
+from connectors.models.assignment_config import AssignmentConfig
 from connectors.models.test_files import TestFiles
 
 
 class AutograderRequest:
     def __init__(
             self,
-            assignment_files: dict,
-            preset: Preset,
+            submission_files: dict,
+            assignment_config: AssignmentConfig,
             student_name,
-            test_framework,
+            student_credentials=None,
             feedback_mode="default",
             openai_key=None,
             redis_url=None,
             redis_token=None):
-        self.assignment_files = assignment_files
-        self.preset = preset
+        self.submission_files = submission_files
+        self.assignment_config = assignment_config
         self.student_name = student_name
-        self.test_framework = test_framework
+        self.student_credentials = student_credentials
         self.feedback_mode = feedback_mode
         self.openai_key = openai_key
         self.redis_url = redis_url
