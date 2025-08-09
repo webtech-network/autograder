aedcfc727ece3b2b95abb0332587c7e9b5e35c26
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-08-09 12:09:09 -0300

    feat: Implemented `status` attribute on AutograderResponse

--- autograder/core/models/autograder_response.py
+++ autograder/core/models/autograder_response.py
@@ -6,10 +6,10 @@ class AutograderResponse:
     Represents the response from the autograder.
     """
 
-    def __init__(self, final_score: float = 0.0, feedback: str = ""):
-        #self.status = status
+    def __init__(self,status, final_score: float = 0.0, feedback: str = ""):
+        self.status = status
         self.final_score = final_score
         self.feedback = feedback
 
     def __repr__(self):
-        return f"AutograderResponse(final_score={self.final_score}, feedback_size={len(self.feedback)})"""
+        return f"AutograderResponse(status={self.status}, final_score={self.final_score}, feedback_size={len(self.feedback)})"""
