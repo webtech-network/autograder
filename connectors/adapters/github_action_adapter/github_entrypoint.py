70aab833fb7f2877db7e3705ea8ddc0499f364f6
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-08-18 07:10:11 -0300

    fix: fixing action entrypoint

--- connectors/adapters/github_action_adapter/github_entrypoint.py
+++ connectors/adapters/github_action_adapter/github_entrypoint.py
@@ -2,9 +2,9 @@ from argparse import ArgumentParser
 from connectors.adapters.github_action_adapter.github_adapter import GithubAdapter
 from connectors.utils.load_preset import load_preset
 parser = ArgumentParser(description="GitHub Action Adapter for Autograder")
-parser.add_argument("--github_token", type=str, required=True, help="GitHub Token")
+parser.add_argument("--github-token", type=str, required=True, help="GitHub Token")
 parser.add_argument("--grading-preset", type=str, required=True, help="The grading preset to use (e.g., api, html, python, etc.)")
-parser.add_argument("--student_name", type=str, required=True, help="The name of the student")
+parser.add_argument("--student-name", type=str, required=True, help="The name of the student")
 parser.add_argument("--feedback_type", type=str, default="default",help="The type of feedback to provide (default or ai)")
 parser.add_argument("--app_token", type=str, required=False, help="GitHub App Token")
 parser.add_argument("--test_framework", type=str, required=False, help="The test framework to use (e.g., pytest)")
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,13 @@ cd /app
 
 tree -a -I "node_modules|.git|.github|.vscode|.idea|.pytest_cache|__pycache__|.DS_Store" .
 
-python connectors/adapters/github_action_adapter/github_entrypoint.py \
+python -m connectors.adapters.github_action_adapter.github_entrypoint.py \
     --github-token $GITHUB_TOKEN \
-    --app_token $APP_TOKEN \
-    --test_framework $TEST_FRAMEWORK \
-    --grading_preset $GRADING_PRESET \
+    --app-token $APP_TOKEN \
+    --test-framework $TEST_FRAMEWORK \
+    --grading-preset $GRADING_PRESET \
     --student-name $GITHUB_ACTOR \
     --feedback-type $FEEDBACK_TYPE \
-    --openai_key $OPENAI_KEY \
-    --redis_url $REDIS_URL \
-    --redis_token $REDIS_TOKEN
+    --openai-key $OPENAI_KEY \
+    --redis-url $REDIS_URL \
+    --redis-token $REDIS_TOKEN
