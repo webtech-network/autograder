71340ad86cb0fa67a4417cb5f45cefe78899bf44
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-08-26 21:05:07 -0300

    feat: Update TestResult class to include report and subject name

--- autograder/core/models/test_result.py
+++ autograder/core/models/test_result.py
@@ -1,9 +1,11 @@
 
 class TestResult:
-    def __init__(self, test_name: str, score: int, message: str = ""):
+    """Stores the outcome of a single test execution from the test library."""
+    def __init__(self, test_name: str, score: int, report: str, subject_name: str = ""):
         self.test_name = test_name
         self.score = score
-        self.message = message
+        self.report = report
+        self.subject_name = subject_name # Added reference to the subject
 
     def __repr__(self):
-        return f"<TestResult {self.test_name}: score->{self.score} - {self.message}>"
+        return f"TestResult(subject='{self.subject_name}', name='{self.test_name}', score={self.score})"
