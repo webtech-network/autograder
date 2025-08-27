f38058c0a69163d375c361b6e62548828368a459
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-08-27 09:02:38 -0300

    feat: add TemplateLibrary for dynamic template retrieval

---
+++ autograder/builder/template_library/library.py
@@ -0,0 +1,10 @@
+from autograder.builder.template_library.templates.template import Template
+
+
+class TemplateLibrary:
+    @staticmethod
+    def get_template(template_name: str):
+        if template_name == "web dev":
+            from autograder.builder.template_library.templates.web_dev import WebDevLibrary
+            return WebDevLibrary()
+        return None
