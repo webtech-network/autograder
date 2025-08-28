df587209e48027802efe4489f2e6a8cf3015391d
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-08-28 07:59:49 -0300

    feat: add FeedbackPreferences class and update BaseReporter to include feedback options

---
+++ autograder/core/models/feedback_preferences.py
@@ -0,0 +1,5 @@
+
+class FeedbackPreferences:
+    """Class that keeps all the feedback report preferences for both AI and Default reporters."""
+    def __init__(self):
+        pass
--- autograder/core/report/base_reporter.py
+++ autograder/core/report/base_reporter.py
@@ -3,8 +3,9 @@ from abc import ABC, abstractmethod
 
 class BaseReporter(ABC):
     """Abstract base class for reporting test results."""
-    def __init__(self,result):
+    def __init__(self,result,feedback: FeedbackPreferences):
         self.result = result
+        self.feedback_json = feedback
     @abstractmethod
     def generate_feedback(self):
         """Generate feedback based on the test results."""
