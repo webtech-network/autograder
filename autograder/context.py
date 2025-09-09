5338d3ffe9d4b94f300f6b0f0166e05d91e01621
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-09-09 01:03:07 -0300

    feat: add RequestContext singleton for managing active AutograderRequest

---
+++ autograder/context.py
@@ -0,0 +1,31 @@
+from connectors.models.autograder_request import AutograderRequest
+
+
+class RequestContext:
+    """
+    A Singleton class to hold the active AutograderRequest object.
+    This provides a global point of access to request data, avoiding the need
+    to pass the request object through multiple layers of the application.
+    """
+    _instance = None
+
+    @classmethod
+    def get_instance(cls):
+        """Gets the single instance of the class."""
+        if cls._instance is None:
+            cls._instance = cls.__new__(cls)
+            cls._instance.request = None
+        return cls._instance
+
+    def set_request(self, autograder_request: AutograderRequest):
+        """Sets the active autograder request for the current session."""
+        self.request = autograder_request
+
+    def get_request(self):
+        """Gets the active autograder request."""
+        if self.request is None:
+            raise Exception("RequestContext has not been initialized. Call set_request() first.")
+        return self.request
+
+# Create a globally accessible instance
+request_context = RequestContext.get_instance()
