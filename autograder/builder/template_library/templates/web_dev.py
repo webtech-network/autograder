242d175d130589abed817405659bfc70a1e156c2
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2025-09-09 00:21:46 -0300

    feat: add execution helper properties to web development template class

--- autograder/builder/template_library/templates/web_dev.py
+++ autograder/builder/template_library/templates/web_dev.py
@@ -512,7 +512,12 @@ class WebDevTemplate(Template):
     @property
     def template_description(self):
         return "A comprehensive template for web development assignments, including tests for HTML, CSS, and JavaScript."
-
+    @property
+    def requires_execution_helper(self) -> bool:
+        return False
+    @property
+    def execution_helper(self):
+        return None
     @property
     def requires_pre_executed_tree(self) -> bool:
         return False
