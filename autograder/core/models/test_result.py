1f284f5819070d06d9b6efb99817f6e8dd88eee8
joaovitoralvarenga <ggquennbr@gmail.com>
2025-10-23 23:48:45 -0300

    refactor: convertion to pydantic models: autograder_response;test_result;result

--- autograder/core/models/test_result.py
+++ autograder/core/models/test_result.py
@@ -1,17 +1,19 @@
+from pydantic import BaseModel, Field
+from typing import Dict, Any
 
-class TestResult:
+class TestResult(BaseModel):
     """Stores the outcome of a single test execution from the test library."""
-    def __init__(self, test_name: str, score: int, report: str, subject_name: str = "", parameters: dict = None):
-        self.test_name = test_name
-        self.score = score
-        self.report = report
-        self.subject_name = subject_name # Added reference to the subject
-        self.parameters = parameters if parameters is not None else {}
 
-    def get_result(self, *args, **kwargs):
+    test_name: str
+    score: int
+    report: str
+    subjet_name: str = ""
+    paremeters: Dict[str, Any] = Field(default_factory=dict)
+
+    def get_result(self, *args, **kwargs) :
         return [self]
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
         return {
             "test_name": self.test_name,
             "score": self.score,
@@ -19,5 +21,6 @@ class TestResult:
             "subject_name": self.subject_name,
             "parameters": self.parameters
         }
-    def __repr__(self):
+
+    def __rpr__(self):
         return f"TestResult(subject='{self.subject_name}', name='{self.test_name}', score={self.score})"
