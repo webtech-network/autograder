73da41890b58a915ac03bf4c7a098e25ad3812ca
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2026-01-19 11:40:54 -0300

    refactor: update result_tree attribute to be optional in grading result

--- autograder/models/dataclass/grading_result.py
+++ autograder/models/dataclass/grading_result.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import Optional
+from autograder.models.result_tree import ResultTree
 
 
 @dataclass
@@ -7,7 +8,7 @@ class GradingResult:
     final_score: float
     status: str
     feedback: Optional[str] = None
-    result_tree: 'ResultTree' = None
+    result_tree: Optional['ResultTree'] = None
     # In case of error
     error: Optional[str] = None
     failed_at_step: Optional[str] = None
