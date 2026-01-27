893d27805fd150747bf4f453e93c607e0026624c
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2026-01-26 23:24:14 -0300

    feat: enhance pipeline execution flow with PipelineExecution management

--- autograder/pipeline.py
+++ autograder/pipeline.py
@@ -1,6 +1,7 @@
 from autograder.models.dataclass.grading_result import GradingResult
 from autograder.models.abstract.step import Step
-from autograder.models.dataclass.step_result import StepResult, StepStatus
+from autograder.models.dataclass.pipeline_execution import PipelineExecution
+from autograder.models.dataclass.step_result import StepResult, StepStatus, StepName
 
 
 class AutograderPipeline:
@@ -11,20 +12,29 @@ class AutograderPipeline:
         self._steps.append(step)
 
     def run(self, input_data:'Submission'):
-        result = StepResult(data=input_data, status=StepStatus.SUCCESS, original_input=input_data) #Initialize result object with input data
-        print(result)
+        result = StepResult(
+            step=StepName.BOOTSTRAP,
+            data=input_data,
+            status=StepStatus.SUCCESS)
+        #Initialize result object with input data
+        pipeline_execution = PipelineExecution(step_results=[], assignment_id="assignment_123", submission=input_data) #Example assignment_id
+        pipeline_execution.add_step_result(result)
+
         for step in self._steps:
             print("Executing step:", step.__class__.__name__)
-            if not result.is_successful:
+            if not result.get_previous_step.is_successful:
                 break
             try:
-                result = step.execute(result.data)
+                result = step.execute(result)
             except Exception as e:
-                result.error = str(e)
-                result.status = StepStatus.FAIL
-                result.failed_at_step = step.__class__.__name__
+                StepResult(
+                    step=step.__class__.__name__,
+                    data=None,
+                    status=StepStatus.FAIL,
+                    error=str(e),
+                )
 
-        if not result.is_successful:
+        if not result.is_successful: #Change this to report a PipelineExecution error with result details
             return GradingResult( #Maybe return a ErrorResponse object?
                 final_score=0.0,
                 status="error",
@@ -34,7 +44,7 @@ class AutograderPipeline:
                 failed_at_step=result.failed_at_step,
             )
         else:
-            return result.data # Assuming the final step returns a GradingResult
+            return result.get_step_result(StepName.GRADE).data # How to return with feedback? How to know when there's no feedback?
 
 
 
