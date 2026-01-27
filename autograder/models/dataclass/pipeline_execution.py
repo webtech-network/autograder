cbb625064212077782ce7d99a9fcbf8546155c66
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2026-01-26 23:16:47 -0300

    feat: introduce PipelineExecution class to manage pipeline execution and step results

---
+++ autograder/models/dataclass/pipeline_execution.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from typing import List
+
+from autograder.models.dataclass.step_result import StepResult, StepName
+from autograder.models.dataclass.submission import Submission
+
+
+@dataclass
+class PipelineExecution:
+    """
+    Main object of the autograder pipeline, keeps track of the execution and step results.
+
+    Attributes:
+        step_results (list): A list of StepResult objects representing the results of each step in the pipeline.
+        assignment_id (str): The unique identifier for the assignment being graded.
+        submission (Submission): The submission being processed in the pipeline.
+    """
+    step_results: List[StepResult]
+    assignment_id: str
+    submission: Submission
+
+    def add_step_result(self, step_result: StepResult) -> 'PipelineExecution':
+        self.step_results.append(step_result)
+        return self
+
+    def get_step_result(self, step_name: StepName) -> StepResult:
+        for step_result in self.step_results:
+            if step_result.step == step_name:
+                return step_result
+        raise ValueError(f"Step {step_name} was not executed in the pipeline.")
+
+    def get_previous_step(self):
+        return self.step_results[-1] if self.step_results else None
--- autograder/models/dataclass/step_result.py
+++ autograder/models/dataclass/step_result.py
@@ -10,12 +10,22 @@ class StepStatus(Enum):
     FAIL = "fail"
 
 
+class StepName(Enum):
+    BOOTSTRAP = "BootstrapStep"
+    LOAD_TEMPLATE = "LoadTemplateStep"
+    BUILD_TREE = "BuildTreeStep"
+    PRE_FLIGHT = "PreFlightStep"
+    GRADE = "GradeStep"
+    FEEDBACK = "FeedbackStep"
+    EXPORTER = "ExporterStep"
+
+
 @dataclass
 class StepResult(Generic[T]):
+    step: StepName
     data: T
     status: StepStatus = StepStatus.SUCCESS
     error: Optional[str] = None
-    failed_at_step: Optional[str] = None
     original_input: Any = None
 
     @property
