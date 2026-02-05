b674886725ebad85318039b1605b50077c99218e
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2026-02-04 22:16:53 -0300

    feat: implement error handling and return StepResult in feedback_step

--- autograder/steps/feedback_step.py
+++ autograder/steps/feedback_step.py
@@ -1,7 +1,7 @@
 from autograder.models.dataclass.grading_result import GradingResult
 from autograder.models.abstract.step import Step
 from autograder.models.dataclass.pipeline_execution import PipelineExecution
-from autograder.models.dataclass.step_result import StepName
+from autograder.models.dataclass.step_result import StepName, StepResult, StepStatus
 from autograder.services.report.reporter_service import ReporterService
 
 
@@ -20,3 +20,12 @@ class FeedbackStep(Step):
                 grading_result=result_tree,
                 feedback_config=self._feedback_config
             ) #TODO: Implement generate_feedback method @joaovitoralvarenga
+            return input.add_step_result(feedback) #Assuming feedback is a StepResult
+        except Exception as e:
+            return input.add_step_result(
+                StepResult(
+                step=StepName.FEEDBACK,
+                data=None,
+                status=StepStatus.FAIL,
+                error=f"Failed to generate feedback: {str(e)}")
+            )
