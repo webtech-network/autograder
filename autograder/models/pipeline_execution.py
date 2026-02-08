e30fbebf60f3ce01e82db3a76389e029611d3720
ArthurCRodrigues <arthurcarvalhorodrigues2409@gmail.com>
2026-02-08 19:55:03 -0300

    feat: enhance PipelineExecution to include grading results

--- autograder/models/pipeline_execution.py
+++ autograder/models/pipeline_execution.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import List, Optional
 
+from autograder.models.dataclass.grading_result import GradingResult
 from autograder.models.dataclass.step_result import StepResult, StepName, StepStatus
 from autograder.models.dataclass.submission import Submission
 
@@ -27,6 +28,7 @@ class PipelineExecution:
     assignment_id: int
     submission: Submission
     status: PipelineStatus = PipelineStatus.EMPTY
+    result: Optional[GradingResult] = None
 
     def add_step_result(self, step_result: StepResult) -> 'PipelineExecution':
         self.step_results.append(step_result)
@@ -38,21 +40,43 @@ class PipelineExecution:
                 return step_result
         raise ValueError(f"Step {step_name} was not executed in the pipeline.")
 
+    def has_step_result(self, step_name: StepName) -> bool:
+        return any(step_result.step == step_name for step_result in self.step_results)
+
     def get_previous_step(self) -> Optional[StepResult]:
         return self.step_results[-1] if self.step_results else None
 
     def set_failure(self):
         self.status = PipelineStatus.FAILED
 
+    def finish_execution(self):
+        """
+        Set Pipeline status to SUCCESS if it is not already FAILED
+
+        If pipeline succeeds, sets the result object with the final grading result and feedback (if available) for easy retrieval.
+        """
+        grading_result = None
+        if self.status != PipelineStatus.FAILED:
+            self.status = PipelineStatus.SUCCESS
+            grading_result = GradingResult(
+                final_score=self.get_step_result(StepName.GRADE).data.final_score,
+                feedback=self.get_step_result(StepName.FEEDBACK).data if self.has_step_result(StepName.FEEDBACK) else None,
+                result_tree=self.get_step_result(StepName.GRADE).data.result_tree
+            )
+        self.result = grading_result
+
+
     @classmethod
-    def create_pipeline_obj(cls, submission: Submission) -> 'PipelineExecution':
+    def start_execution(cls, submission: Submission) -> 'PipelineExecution':
 
         bootstrap = StepResult(
             step=StepName.BOOTSTRAP,
             data=submission,
             status=StepStatus.SUCCESS)
 
-        pipeline_execution = PipelineExecution(step_results=[], assignment_id=submission.assignment_id,
+        pipeline_execution = PipelineExecution(step_results=[],
+                                               assignment_id=submission.assignment_id,
+                                               status=PipelineStatus.RUNNING,
                                                submission=submission)
 
         pipeline_execution.add_step_result(bootstrap)
