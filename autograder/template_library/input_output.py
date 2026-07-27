5100441869c1f976d74978545f34364ff6fc3e24
Arthur Carvalho <arthurcarvalhorodrigues2409@gmail.com>
2026-07-26 23:22:52 -0300

    Change program_command to 'quebra' in input_output.py

--- autograder/template_library/input_output.py
+++ autograder/template_library/input_output.py
@@ -132,7 +132,7 @@ class ExpectOutputTest(BaseExecutionTest):
             output = self.run_sandbox_execution(
                 sandbox=sandbox,
                 inputs=inputs,
-                program_command=program_command,
+                program_command="quebra",
             )
 
             # Check for generic execution failures
